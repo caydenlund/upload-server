5c6438c4b57c074963e6a463ecaf42bc6aa783ea
shrimpster00 <caydenlund@gmail.com>
2021-06-22 13:34:11 -0600

    Flesh out fully-functional prototype.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require("http");
 // Import npm libraries.
 const express = require("express");
 const serveIndex = require("serve-index");
+const multer = require("multer");
 
 // Establish defaults.
 const DEFAULT_PORT = 3000;
@@ -15,6 +16,23 @@ const FILE_DIRECTORY = "files";
 
 // Create Express server.
 const app = express();
+
+// Establish Multer middleware.
+const storage = multer.diskStorage({
+    destination: (req, file, next) => {
+        next(null, FILE_DIRECTORY);
+    },
+    filename: (req, file, next) => {
+        next(null, file.originalname);
+    }
+});
+const upload = new multer({storage});
+
+// Upload files at "/upload".
+app.post("/upload", upload.array("files"), (req, res, next) => {
+    res.redirect("/");
+    res.end();
+});
 
 // Serve static files out of "public" at "/".
 app.use(express.static("public"));
