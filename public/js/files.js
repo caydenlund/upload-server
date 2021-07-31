363984828a4c7725ae4d4850a2e4772de899e1ca
cayden <caydenlund@gmail.com>
2021-07-31 04:57:16 -0600

    Fix some code formatting issues.

--- public/js/files.js
+++ public/js/files.js
@@ -1,17 +1,17 @@
 function sendDelete(event) {
-    event.preventDefault();
-    const target = event.currentTarget.dataset.target;
+  event.preventDefault();
+  const target = event.currentTarget.dataset.target;
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('DELETE', `/files/${target}`);
-    xhr.send();
+  const xhr = new XMLHttpRequest();
+  xhr.open("DELETE", `/files/${target}`);
+  xhr.send();
 
-    xhr.addEventListener("error", function(err) {
-        // TODO: Handle error
-        console.log(err);
-    });
+  xhr.addEventListener("error", function (err) {
+    // TODO: Handle error
+    console.log(err);
+  });
 
-    xhr.addEventListener("load", function() {
-        window.location.reload();
-    });
+  xhr.addEventListener("load", function () {
+    window.location.reload();
+  });
 }
